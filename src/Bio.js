a8f6f75cd5e932080a927ee5ce948d3fbee9e1c9
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2020-12-15 12:39:33 +0530

    Update Bio.js

--- src/Bio.js
+++ src/Bio.js
@@ -17,7 +17,7 @@ const Bio = () => {
         <Col xs={9} md={9} lg={9} className="main-bio mt-2">
           <p>
             <span role="img" aria-label="">
-             Software Engineer at InnoWise LLC • ReactJs ⚛️ • Full Stack 🌏{" "}
+             Software Engineer • ReactJs ⚛️ • Full Stack 🌏{" "}
             </span>
             <br />
             Hi
