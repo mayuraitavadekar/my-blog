0ca2095545deae8ac8984f8d4730756280d99398
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2021-05-27 00:32:59 +0530

    Add files via upload

--- src/articles/posts.js
+++ src/articles/posts.js
@@ -1,7 +1,5 @@
-import A1 from "../articles/18-june-2020.mdx";
-import A2 from "../articles/25-september-2020.mdx";
-import A3 from "../articles/11-april-2021.mdx";
-
+import A1 from "../articles/11-april-2021.mdx";
+import A2 from "../articles/26-may-2021.mdx";
 export const readTime = async (path) => {
   const res = await fetch(path)
     .then((response) => response.text())
@@ -13,27 +11,35 @@ export const readTime = async (path) => {
 };
 
 export const posts = [
+  // {
+  //   number: "1",
+  //   title: "UseState, UseEffect - Most popular hooks?",
+  //   description: "Personal thoughts on react hooks.",
+  //   path: A1,
+  //   tags: ["react", "react-hooks"],
+  //   date: "18 June, 2020",
+  // },
+  // {
+  //   number: "2",
+  //   title: "Quick steps for Quicksort",
+  //   description: "Small and clean notes on quick sort.",
+  //   path: A2,
+  //   tags: ["data-structures", "algorithms"],
+  //   date: "25 September, 2020",
+  // },
   {
     number: "1",
-    title: "UseState, UseEffect - Most popular hooks?",
-    description: "Personal thoughts on react hooks.",
+    title: "3 years with JavaScript: What I learned?",
+    description: "My personal experiences with the programming language.",
     path: A1,
-    tags: ["react", "react-hooks"],
-    date: "18 June, 2020",
+    tags: ["programming"],
+    date: "11 April, 2021",
   },
   {
     number: "2",
-    title: "Quick steps for Quicksort",
-    description: "Small and clean notes on quick sort.",
+    title: "Which tech-stack do we use at Educulture?",
+    description: "Sharing my experience in building Educulture server-side.",
     path: A2,
-    tags: ["data-structures", "algorithms"],
-    date: "25 September, 2020",
-  },
-  {
-    number: "3",
-    title: "3 years with JavaScript: What I learned?",
-    description: "My personal experiences with the programming language.",
-    path: A3,
     tags: ["programming"],
     date: "11 April, 2021",
   },
