d93c81aa2f545a00e02acf4188cc0fddc6cb12db
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2021-05-31 18:55:17 +0530

    Update posts.js

--- src/articles/posts.js
+++ src/articles/posts.js
@@ -4,7 +4,7 @@ export const readTime = async (path) => {
   const res = await fetch(path)
     .then((response) => response.text())
     .then((text) => {
-      return Math.ceil(text.length / 800).toString();
+      return Math.ceil(text.length / 900).toString();
     });
 
   return res;
